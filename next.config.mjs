c77d2bd4552f3fd27c363284c2d125c578cace8a
panditajay73 <ajaypandey91700@gmail.com>
2026-07-13 22:11:09 +0530

    Ignore ESLint during production build

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,26 @@
 /** @type {import('next').NextConfig} */
 const isGithubPages = process.env.DEPLOY_TARGET === "gh-pages";
-// If deploying to https://<user>.github.io/<repo>/, set NEXT_PUBLIC_BASE_PATH="/<repo>"
+
+// If deploying to https://<user>.github.io/<repo>/,
+// set NEXT_PUBLIC_BASE_PATH="/<repo>"
 const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
 
 const nextConfig = {
   output: "export",
-  images: { unoptimized: true },
+
+  images: {
+    unoptimized: true,
+  },
+
   trailingSlash: true,
+
   basePath: isGithubPages ? basePath : "",
+
   assetPrefix: isGithubPages ? basePath : undefined,
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
